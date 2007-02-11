9daa158524adf634b21a1a6e1985f329aad8a535
Hanno Schlichting <hanno@hannosch.eu>
2007-02-11 20:03:39 +0000

    Forgot the setuptools dependency
    svn path=/plone.fieldsets/trunk/; revision=12373

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='plone.fieldsets',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+        'setuptools',
       ],
       entry_points="""
       # -*- Entry points: -*-
