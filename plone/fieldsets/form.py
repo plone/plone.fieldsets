4c0664434fc7cd4416eca9cb4c37dde694529102
Duncan Booth <Duncan.Booth@suttoncourtenay.org.uk>
2007-02-20 15:55:50 +0000

    When using fieldsets build the full widget list from the fieldset widget lists so that state is correctly shared.
    svn path=/plone.fieldsets/trunk/; revision=12871

--- plone/fieldsets/form.py
+++ plone/fieldsets/form.py
@@ -11,18 +11,24 @@ class FieldsetsEditForm(EditForm):
         # First part is copied from zope.formlib.form.EditForm licensed under
         # the ZPL 2.1
         self.adapters = {}
-        self.widgets = form.setUpEditWidgets(
-            self.form_fields, self.prefix, self.context, self.request,
-            adapters=self.adapters, ignore_request=ignore_request
-            )
         # In order to support fieldsets, we need to setup the widgets on all
         # the fieldsets as well.
         if self.is_fieldsets():
+            self.widgets = None
             for fieldset in self.form_fields.fieldsets:
                 fieldset.widgets = form.setUpEditWidgets(
                     fieldset, self.prefix, self.context, self.request,
                     adapters=self.adapters, ignore_request=ignore_request
                     )
+                if self.widgets is None:
+                    self.widgets = fieldset.widgets
+                else:
+                    self.widgets += fieldset.widgets
+        else:
+            self.widgets = form.setUpEditWidgets(
+                self.form_fields, self.prefix, self.context, self.request,
+                adapters=self.adapters, ignore_request=ignore_request
+                )
 
     def is_fieldsets(self):
         # We need to be able to test for non-fieldsets in templates.
