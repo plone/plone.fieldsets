0ca26441c886aff30f9c792e5ca5680d2d4996fa
Hanno Schlichting <hanno@hannosch.eu>
2010-07-16 13:40:02 +0000

    Update license to GPL version 2 only.
    svn path=/plone.fieldsets/trunk/; revision=37889

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-import os
 from setuptools import setup, find_packages
 
-version = '2.0a2'
+version = '2.0'
 
 setup(name='plone.fieldsets',
       version=version,
       description="An extension to zope.formlib, which allows to group fields into different fieldsets.",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
-        "Programming Language :: Python",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
         ],
       keywords='zope formlib fieldsets',
       author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://pypi.python.org/pypi/plone.fieldsets',
-      license='GPL',
+      license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone'],
       include_package_data=True,
